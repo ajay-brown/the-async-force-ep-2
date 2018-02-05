9afcfe12ac74083135e32e6405051ea3b798e59c
Audrey <audreybrown94@gmail.com>
2018-02-04 14:29:45 -1000

    buttons functional

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
 window.addEventListener("load", startup);
+
 function startup() {
-  document.getElementById("requestResourceButton").addEventListener(
-    "click",
-    function() {
-      document.querySelectorAll(
-        "select[name='resourceType']"
-      ).onchange = changeEventHandler();
-    },
-    false
-  );
-}
-var resources = document.getElementById("resourceType");
-function changeEventHandler() {
-  if (resources.value == "people") {
-    let peopleId = document.getElementById("resourceId");
-    console.log(peopleId);
-    console.log("people");
-    const oReq1 = new XMLHttpRequest();
-    let URLhost = "https://swap.co/api/";
-    oReq1.addEventListener("onclick", person4Listener);
-    oReq1.open("GET", "https://swapi.co/api/people/4/");
-    oReq1.send();
+  document
+    .getElementById("requestResourceButton")
+    .addEventListener("click", function(e) {
+      if (resourceType === "people") {
+        getPeople(resourceId);
+      } else if ((resourceType = "planets")) {
+        getPlanets(resourceId);
+      } else {
+        getStarships(resourceId);
+      }
+    });
+  let resourceType = document.getElementById("resourceType").value;
+  let resourceId = document.getElementById("resourceId").value;
+  console.log(resourceId);
+  function getPeople(resourceId) {
+    let newReq = new XMLHttpRequest();
+    newReq.addEventListener("load", function(e) {
+      let peopleData = JSON.parse(e.target.responseText);
+      console.log(peopleData); //showing array of all people
+    });
+    newReq.open("GET", "https://swapi.co/api/people/?page=" + resourceId);
+    newReq.send();
   }
-  if (resources.value === "planets") {
-    console.log("planets");
-    const oReq2 = new XMLHttpRequest();
-    let URLhost = "https://swap.co/api/";
-    oReq2.addEventListener("onclick", person4Listener);
-    oReq2.open("GET", "https://swapi.co/api/people/4/");
-    oReq2.send();
-  }
-  if (resources.value === "starships") {
-    console.log("starships");
-    const oReq3 = new XMLHttpRequest();
-    let URLhost = "https://swap.co/api/";
-    oReq3.addEventListener("onclick", person4Listener);
-    oReq3.open("GET", "https://swapi.co/api/people/4/");
-    oReq3.send();
-  }
+  function getPlanets(resourceId) {}
+  function getStarships(resourceId) {}
 }
