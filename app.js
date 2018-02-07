fcc6b76d4996b829cfe6013bce967eb322d9a58f
Audrey <audreybrown94@gmail.com>
2018-02-06 18:44:27 -1000

    new way of organizing in new branch

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-window.addEventListener("load", startup);
-
-function startup() {
-  document
-    .getElementById("requestResourceButton")
-    .addEventListener("click", function(e) {
-      if (resourceType === "people") {
-        getPeople(resourceId);
-      } else if ((resourceType = "planets")) {
-        getPlanets(resourceId);
-      } else {
-        getStarships(resourceId);
-      }
-    });
-  let resourceType = document.getElementById("resourceType").value;
-  let resourceId = document.getElementById("resourceId").value;
-  console.log(resourceId);
-  function getPeople(resourceId) {
-    let newReq = new XMLHttpRequest();
-    newReq.addEventListener("load", function(e) {
-      let peopleData = JSON.parse(e.target.responseText);
-      console.log(peopleData); //showing array of all people
-    });
-    newReq.open("GET", "https://swapi.co/api/people/?page=" + resourceId);
-    newReq.send();
-  }
-  function getPlanets(resourceId) {}
-  function getStarships(resourceId) {}
+function getData() {
+  const oReq = newXMLHttpRequest();
+  oReq.addEventListener("load", function() {
+    const data = JSON.parse(this.responseText);
+  });
+  oReq.open("GET", "https://swapi.co/api/");
+  oReq.send();
 }
+getData();
